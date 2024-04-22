d85ff827c0ea6560f92391c4bb2cc6181f577ba4
Jeffs-Soares <soares.jeffersonthadeu@gmail.com>
2024-04-22 09:42:03 -0300

    show budget status

--- resources/views/budget/index.blade.php
+++ resources/views/budget/index.blade.php
@@ -17,6 +17,10 @@
                 <th scope="col" class="px-6 py-3">
                     Action
                 </th>
+
+                <th scope="col" class="px-6 py-3">
+                    Status
+                </th>
             </tr>
             </thead>
             <tbody>
@@ -45,6 +49,25 @@
 
                     <a class="font-medium text-green-600 dark:text-green-500 hover:underline" href={{route('payCreate', $budget->id)}}> <button> Pay </button> </a>
                 </td>
+
+
+                <td class="px-6 py-4">
+                    @if($budget->remnant == $budget->total_value)
+                        <span class="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-red-400 border border-red-400">Unpaid</span>
+                    @endif
+
+                    @if($budget->remnant == 0)
+                            <span class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400">Paid out</span>
+                    @endif
+
+                    @if($budget->remnant > 0 && $budget->remnant < $budget->total_value)
+                            <span class="bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-yellow-300 border border-yellow-300">Partially Paid out </span>
+                    @endif
+
+                </td>
+
+
+
             </tr>
 
         @endforeach
