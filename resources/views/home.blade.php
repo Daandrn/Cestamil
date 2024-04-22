8ad1fc1318fc1e007c9e9c949978cc7354ba0968
Jeffs-Soares <soares.jeffersonthadeu@gmail.com>
2024-04-22 09:52:52 -0300

    add english on dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
 
     <a href="" class="block w-max col-start-1 col-span-2">
         <div class="p-2 px-4 h-[200px] w-[200px] bg-gray-200 rounded-lg dark:bg-gray-800 flex flex-col gap-6 relative">
-            <h2 class="text-gray-50 text-center"> Orçamentos Totalmente Pagos </h2>
+            <h2 class="text-gray-50 text-center"> Fully-paid budgets </h2>
 
             <p class="text-center text-gray-50 text-5xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"> {{ $totallyPaid }} </p>
 
@@ -15,7 +15,7 @@
 
     <a href="" class="block w-max col-start-3 col-span-2">
         <div class="p-2 px-4 h-[200px] w-[200px] bg-gray-200 rounded-lg dark:bg-gray-800 flex flex-col gap-6 relative">
-            <h2 class="text-gray-50 text-center"> Orçamentos Parcialmente Pagos </h2>
+            <h2 class="text-gray-50 text-center"> Partially Paid Budgets </h2>
 
             <p class="text-center text-gray-50 text-5xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"> {{ $partiallyPaid  }} </p>
 
@@ -24,7 +24,7 @@
 
     <a href="" class="block w-max col-start-5 col-span-2">
         <div class="p-2 px-4 h-[200px] w-[200px] bg-gray-200 rounded-lg dark:bg-gray-800 flex flex-col gap-6 relative">
-            <h2 class="text-gray-50 text-center"> Orçamentos não pagos </h2>
+            <h2 class="text-gray-50 text-center"> Unpaid budgets </h2>
 
             <p class="text-center text-gray-50 text-5xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 "> {{ $notTotallyPaid  }} </p>
 
@@ -33,7 +33,7 @@
 
         <a href="" class="block w-max col-start-5 col-span-2">
             <div class="p-2 px-4 h-[200px] w-[200px] bg-gray-200 rounded-lg dark:bg-gray-800 flex flex-col gap-6 relative">
-                <h2 class="text-gray-50 text-center"> Último orçamento feito </h2>
+                <h2 class="text-gray-50 text-center"> Last budget </h2>
 
                 <p class="text-center text-gray-50 text-2xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 "> {{  $lastDate  }} </p>
 
